5be84dcc216d6e9c520cd233c6239486b7903899
wlrfernando <wlrfernando@outlook.com>
2024-03-20 16:18:39 +0530

    misc: refine variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,17 @@ data "aws_region" "current" {}
 
 module "vpc" {
   source               = "./modules/network"
-  vpc_name             = "dev"
+  vpc_name             = var.vpc.name[0]
   region               = data.aws_region.current.name
-  vpc_cidr_block       = "10.0.0.0/16"
-  private_subnet_cidrs = ["10.0.2.0/24", "10.0.4.0/24"]
-  public_subnet_cidrs  = ["10.0.10.0/24", "10.0.20.0/24"]
+  vpc_cidr_block       = var.vpc.cidr[0]
+  private_subnet_cidrs = var.vpc.private_subnet_cidrs
+  public_subnet_cidrs  = var.vpc.public_subnet_cidrs
 }
 
 module "alb" {
   source           = "./modules/alb"
   public_subnet_id = [for subnet in module.vpc.public_subnets : subnet.id]
-  alb_name         = "wireapps-alb"
+  alb_name         = "${var.name_prefix}-alb"
   region           = data.aws_region.current.name
   vpc_id           = module.vpc.vpc_id
 
@@ -20,30 +20,30 @@ module "alb" {
 
 module "rds" {
   source             = "./modules/rds"
-  db_name            = "wireappsdb"
+  db_name            = "${var.name_prefix}db"
   db_username        = var.db_username
   db_password        = var.db_password
-  ingress_cidr       = "10.0.0.0/16"
+  ingress_cidr       = var.vpc.cidr[0]
   private_subnet_ids = [for subnet in module.vpc.public_subnets : subnet.id]
   vpc_id             = module.vpc.vpc_id
 }
 
 module "cluster" {
   source       = "./modules/cluster"
-  cluster_name = "wireapps-cluster"
+  cluster_name = "${var.name_prefix}-cluster"
 }
 
 module "web_service" {
   source              = "./modules/fargate"
   docker_context_path = "../web/"
-  app_name            = "wireapps-web"
+  app_name            = "${var.name_prefix}-web"
   api_host            = "http://${module.alb.alb-dns}"
   db_database         = ""
   db_host             = ""
   db_password         = ""
   db_user             = ""
   container_port      = 3000
-  ingress_cidr        = "10.0.0.0/16"
+  ingress_cidr        = var.vpc.cidr[0]
   vpc_id              = module.vpc.vpc_id
   cluster_id          = module.cluster.id
   listener_arn        = module.alb.listener-arn
@@ -51,20 +51,19 @@ module "web_service" {
   app_context_path    = "/*"
   public_subnet_id    = [for subnet in module.vpc.public_subnets : subnet.id]
   health_check_path   = "/"
-  depends_on          = []
 }
 
 module "api_service" {
   source              = "./modules/fargate"
   docker_context_path = "../api/"
-  app_name            = "wireapps-api"
+  app_name            = "${var.name_prefix}-api"
   api_host            = "http://${module.alb.alb-dns}"
   db_database         = "postgres"
   db_host             = module.rds.rds_endpoint
   db_password         = var.db_password
   db_user             = var.db_username
   container_port      = 3000
-  ingress_cidr        = "10.0.0.0/16"
+  ingress_cidr        = var.vpc.cidr[0]
   vpc_id              = module.vpc.vpc_id
   cluster_id          = module.cluster.id
   listener_arn        = module.alb.listener-arn
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,18 @@ variable "db_username" {
 variable "db_password" {
   type = string
 }
+
+variable "name_prefix" {
+  type = string
+  default = "wireapps"
+}
+
+variable "vpc" {
+  type = map(any)
+  default = {
+    name = ["dev"]
+    cidr = ["10.0.0.0/16"]
+    private_subnet_cidrs = ["10.0.2.0/24", "10.0.4.0/24"]
+    public_subnet_cidrs  = ["10.0.10.0/24", "10.0.20.0/24"]
+  }
+}
